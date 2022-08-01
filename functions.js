0fcd2a01a6a5b4abeea579cc1d5317cca809c18f
Bill Stiber <billstiber@gmail.com>
2022-08-01 15:39:21 -0400

    exercise functions

---
+++ functions.js
@@ -0,0 +1,12 @@
+// functions group multiple lines of code under a single name
+// when you run a function you can provide the function with data
+
+var todos = ['Item 1', 'Item 2', 'Item 3'];
+
+function items() {
+  var todos = ['Item 1', 'Item 2', 'Item 3'];
+  console.log(todos);
+}
+
+items();
+
--- todoList.js
+++ todoList.js
@@ -1,4 +1,17 @@
 //copying the code to js file to use quokka
 
 var todos = ['Item 1', 'Item 2', 'Item 3'];
-todos;
+// todos;
+
+// 1. Functions group multiple lines of code together under a single name
+// 2. when you run a function, you can provide the function with data
+
+// create a funtion that would add "watch the next video in Practical Javascript' to the end of the array
+function add (myData) {
+   todos.push(myData); // pushes myData 'value' to the end of the todos array
+   console.log(myData);
+}
+
+add('watch the next video in Practical Javascript');
+console.log(todos);
+
